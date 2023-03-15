9f38b8407ba58f0b24cae9fb9907b7ac0d720a6c
Dizzy01banks <127908815+Dizzy01banks@users.noreply.github.com>
2023-03-14 21:32:06 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,28 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Login Page</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
-	<header>
-		<h1>Login to Grading System</h1>
-	</header>
-	<main>
-		<section>
-			<form method="post" action="login.php">
-				<label for="username">Username:</label>
-				<input type="text" id="username" name="username" required>
-				<label for="password">Password:</label>
-				<input type="password" id="password" name="password" required>
-				<button type="submit">Login</button>
-			</form>
-		</section>
-	</main>
-	<footer>
-		<p>© 2023 Grading System. All Rights Reserved.</p>
-	</footer>
-</body>
-</html>
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if ($username == 'Admin' && $password == 'Diz1') {
+	// Successful login, redirect to student dashboard
+	header('Location: dashboard.php');
+	exit();
+} else {
+	// Invalid login, redirect back to landing page with error message
+	header('Location: index.php?login_error=Invalid username or password.');
+	exit();
+}
+
+?>
