4d93e033c6ff90997ced104b36d89a4d0221db95
Dizzy01banks <127908815+Dizzy01banks@users.noreply.github.com>
2023-03-15 07:26:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,33 @@ function login() {
     alert('Invalid credentials. Please try again.');
   }
 }
+// Function to read data from CSV file
+function readCSVFile() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      processData(xhr.responseText);
+    }
+  }
+  xhr.open('GET', 'grades.csv', true);
+  xhr.send();
+}
+
+// Function to process the data from CSV file
+function processData(csvData) {
+  var rows = csvData.split('\n');
+  var tableHtml = '<table>';
+  for (var i = 0; i < rows.length; i++) {
+    var row = rows[i].split(',');
+    tableHtml += '<tr>';
+    for (var j = 0; j < row.length; j++) {
+      tableHtml += '<td>' + row[j] + '</td>';
+    }
+    tableHtml += '</tr>';
+  }
+  tableHtml += '</table>';
+  document.getElementById('grades-table').innerHTML = tableHtml;
+}
+
+// Add event listener to "grades" button
+document.getElementById('grades-btn').addEventListener('click', readCSVFile);
