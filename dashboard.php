6d56e9b993819b13e1f8cc990c0a2d0633148088
Dizzy01banks <127908815+Dizzy01banks@users.noreply.github.com>
2023-03-14 21:56:24 -0400

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,24 +1,75 @@
+<?php
+session_start();
+if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['portal'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $portal = $_POST['portal'];
+
+    switch ($portal) {
+        case 'parent':
+            if (($username == 'Parent1' && $password == 'Pass1') || ($username == 'Parent2' && $password == 'Pass2')) {
+                $_SESSION['username'] = $username;
+            } else {
+                header('Location: login.php?error=invalid');
+                exit();
+            }
+            break;
+        case 'student':
+            if (($username == 'Student1' && $password == 'Go123') || ($username == 'Student2' && $password == 'Stop123')) {
+                $_SESSION['username'] = $username;
+            } else {
+                header('Location: login.php?error=invalid');
+                exit();
+            }
+            break;
+        case 'teacher':
+            if ($username == 'Teacher' && $password == 'Teach1') {
+                $_SESSION['username'] = $username;
+            } else {
+                header('Location: login.php?error=invalid');
+                exit();
+            }
+            break;
+        default:
+            header('Location: login.php?error=invalid');
+            exit();
+    }
+} else {
+    header('Location: login.php?error=invalid');
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Grading System - Student Dashboard</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="style.css">
+	<title>Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="style-navy-orange.css">
 </head>
 <body>
 	<header>
-		<h1>Grading System - Student Dashboard</h1>
+		<h1>Welcome to the Dashboard</h1>
+		<p>Logged in as <?php echo $_SESSION['username']; ?></p>
 	</header>
 	<main>
-		<section>
-			<h2>Welcome to your dashboard, [Student Name]!</h2>
-			<p>Here you can view your grades, assignments, and progress for all of your classes.</p>
-			<!-- Dashboard content here -->
-		</section>
-	</main>
-	<footer>
-		<p>© 2023 Grading System. All Rights Reserved.</p>
-	</footer>
-</body>
-</html>
+		<?php if ($_POST['portal'] == 'parent'): ?>
+			<section>
+				<h2>Grades</h2>
+				<p>View your child's grades below:</p>
+				<table>
+					<thead>
+						<tr>
+							<th>Course</th>
+							<th>Grade</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>English</td>
+							<td>85%</td>
+						</tr>
+						<tr>
+							<td>Math</td>
+							<td>92%</td>
+						</tr>
+						<tr>
