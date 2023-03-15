aad731fe9a05e9f027aab6ec90b91911c7635dc7
Dizzy01banks <127908815+Dizzy01banks@users.noreply.github.com>
2023-03-15 11:49:25 -0400

    Create router.js

---
+++ router.js
@@ -0,0 +1,23 @@
+// Check if user is logged in
+function checkLoggedIn() {
+  // code to check if user is logged in
+  // return true if logged in, false otherwise
+  return true;
+}
+
+// Redirect to login page if not logged in
+function redirectToLogin() {
+  window.location.href = 'login.html';
+}
+
+// Access student portal link
+function accessStudentPortal() {
+  // Check if user is logged in
+  if (checkLoggedIn()) {
+    // Redirect to student portal page
+    window.location.href = 'student_portal.html';
+  } else {
+    // Redirect to login page
+    redirectToLogin();
+  }
+}
