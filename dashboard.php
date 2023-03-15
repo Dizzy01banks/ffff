278a57f0d07941b61630ef51e6a94a875d08d290
Dizzy01banks <127908815+Dizzy01banks@users.noreply.github.com>
2023-03-14 21:30:13 -0400

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Grading System - Student Dashboard</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>Grading System - Student Dashboard</h1>
+	</header>
+	<main>
+		<section>
+			<h2>Welcome to your dashboard, [Student Name]!</h2>
+			<p>Here you can view your grades, assignments, and progress for all of your classes.</p>
+			<!-- Dashboard content here -->
+		</section>
+	</main>
+	<footer>
+		<p>© 2023 Grading System. All Rights Reserved.</p>
+	</footer>
+</body>
+</html>
